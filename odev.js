109abffb96d0eb864b8132ae72375f3313cdcada
Emrah Sasmaz <emrah_sasmaz1903@outlook.com>
2020-12-31 14:15:37 +0000

    fiveth-week-completed

--- odev.js
+++ odev.js
@@ -33,4 +33,59 @@ document.querySelector("#reset").addEventListener("click", () => {
 
 /* START CODING HERE */
 
+document.querySelector("#populationBigger").addEventListener("click", () => {
 
+   const bt500 = data.filter(city => city.population > 500000);
+   createTableElements(bt500,"allcities");
+  
+});
+
+// Land Area Button
+
+document.querySelector("#landAreaLess").addEventListener("click",() => {
+   const la1000 = data.filter(city => city.landArea <= 1000);
+   createTableElements(la1000,"allcities");
+   
+});
+
+// Population Less Than 100 000 
+
+document.querySelector("#isPopulationLess").addEventListener("click", () => {
+    const lt100000 = data.some(city => city.population <= 100000);
+    if(lt100000) {
+        alert("YES");
+    }else {
+        alert("NO");
+    }
+});
+
+// Land Area Bigger Than 100
+
+document.querySelector("#isLandBigger").addEventListener("click", () => {
+    const laBig100 = data.every(city => city.landArea >100);
+    if(laBig100) {
+        alert("YES");
+    }else {
+        alert("NO");
+    }
+});
+
+// Select City
+
+const cityName = data.map(cityName => cityName.name);
+const citySelect = document.querySelector(".custom-select");
+cityName.forEach((element) => {
+    
+    const cityCreate = document.createElement("option");
+    cityCreate.setAttribute("value",element);
+    cityCreate.textContent = element;
+    citySelect.appendChild(cityCreate);
+    
+});
+
+// Found Item 
+
+citySelect.addEventListener("change", (e) => {
+    const selectCities = data.filter(cities => e.target.value === cities.name);
+    createTableElements(selectCities, "singlecity");
+});
